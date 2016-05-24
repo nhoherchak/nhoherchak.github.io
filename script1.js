75957ab9e2e7ac3f768257121bcc7ec0012a87ab
nhoherchak <natashahoherchak@calvisitor-10-105-144-35.calvisitor.1918.berkeley.edu>
2016-05-23 18:21:55 -0700

    navigation bar

--- script1.js
+++ script1.js
@@ -1,2 +1,9 @@
 document.getElementById("foot01").innerHTML =
 "<p>&copy;  " + new Date().getFullYear() + " Natasha Hoherchak. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='customers.html'>Data</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
