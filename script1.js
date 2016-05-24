fc7ddf511cd8f8767113d8d98693e9b9324f1c49
nhoherchak <natashahoherchak@airbears2-10-142-8-123.airbears2.1918.berkeley.edu>
2016-05-24 15:17:04 -0700

    design

--- script1.js
+++ script1.js
@@ -1,5 +1,5 @@
 document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " Natasha Hoherchak. All rights reserved.</p>";
+"<p>&copy;  " + new Date().getFullYear() + " Natasha Lahiri Hoherchak. All rights reserved.</p>";
 
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
