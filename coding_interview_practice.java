79fe1462424104be1d4a39a8bf502676eba31eba
nhoherchak <nhoherchak@gmail.com>
2016-08-28 16:58:09 -0700

    adding soundscape personas

---
+++ coding_interview_practice.java
@@ -0,0 +1,20 @@
+//comments in java
+
+/* Compute fib sequence for nth number */
+public static long fib (int n) {
+	if (n == 1) {
+		return 1;
+	} else {
+		return fib(n-1) + fib(n-2);
+	}
+}
+
+/* Print out the grade-school multiplication table up to 12x12 (max) */
+public static void mult_tables (int max) {
+	int i = 0; int j = 0;
+	for (int i = 0; i <= max; i++) {
+		for (int j = 0; j <=max; j++) {
+
+		}
+	}
+}
